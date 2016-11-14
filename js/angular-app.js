34f6b4ba42bf6134846c06b01c4c4cad151e048d
jcchv <jcchv92@gmail.com>
2016-11-13 20:17:40 -0700

    AngularJS
    Controlador Tabs

---
+++ js/angular-app.js
@@ -0,0 +1,35 @@
+/* ==========================================================================
+   Wrapping JavaScript in a closure is a good Habit!
+   ========================================================================== */
+(function(){
+    var app = angular.module('myPortfolio', []);
+    app.controller('TabController', function(){
+        this.tab = 1;
+        //$('html, body').animate({ scrollTop: 0 }, 'fast');
+        this.setTab = function(newValue){
+            this.tab = newValue;
+            $('html, body').animate({ scrollTop: 0 }, 'fast');
+        };
+        this.isSet = function(tabName){
+            return this.tab === tabName;
+        };
+    });
+
+    app.controller("HomeController", function($scope) {
+
+    });
+
+    app.controller("AboutController", function($scope) {
+
+
+    });
+
+    app.controller("WorkController", function($scope) {
+
+    });
+
+    app.controller("ContactController", function($scope) {
+
+    });
+
+})();
